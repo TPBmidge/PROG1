55be33f2299ae5c850451d926f5e96a1a7a18656
Manuel <Manuel@ManusNotebook>
2021-11-21 10:44:11 +0100

    Test2

--- Blatt6/src/Hausaufgabe30.java
+++ Blatt6/src/Hausaufgabe30.java
@@ -14,7 +14,6 @@ public class Hausaufgabe30 {
 		//int zeilenAnzahl = Integer.parseInt(JOptionPane.showInputDialog("Anzahl Zeilen"));
 		//paintSchach(spaltenAnzahl*40,zeilenAnzahl*40);
 		paintSchach800(95,160);
-		//Test3ffff
 		
 		
 	}
